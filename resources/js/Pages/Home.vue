b30b5a8c1dbfe018319dd09be2568065508cd144
Sch39 <sukronraezpactoer@gmail.com>
2024-07-04 09:49:48 +0700

    tambah inertiajs/vue3, vite-plugin-vue-devtools dan talwindcss

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <Head>
+        <title>My app</title>
+        <meta head-key="description" name="description" content="This is the default description" />
+        <!-- <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> -->
+    </Head>
+
+    <div>
+        <h1 class="text-3xl font-bold underline text-center">Welcome to Inertia.js with Vue 3 and Laravel 10</h1>
+    </div>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
     plugins: [
@@ -7,5 +10,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
+        vueDevTools({ appendTo: 'app.js' }),
     ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js')
+        }
+    }
 });
