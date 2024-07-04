f70de1ffca54ca646feacb6f9a55b0c0ad4b08d1
Sch39 <sukronraezpactoer@gmail.com>
2024-07-04 10:30:37 +0700

    tambah lib @heroicons/vue, @headlessui/vue, buat default layout  dan component header

---
+++ resources/js/Layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from '../Components/TheHeader.vue';
+</script>
+
+
+<template>
+    <TheHeader />
+    <main>
+        <slot />
+    </main>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import DefaultLayout from './Layouts/default.vue'
 
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         const page = pages[`./Pages/${name}.vue`]
-        // page.default.layout = page.default.layout || Layout
+        page.default.layout = page.default.layout || DefaultLayout
         return page
     },
     setup({ el, App, props, plugin }) {
