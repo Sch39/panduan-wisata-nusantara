29e0ce45107b6acd2b20138ba4a3bd10fe6201a9
Sch39 <sukronraezpactoer@gmail.com>
2024-07-07 17:07:35 +0700

    buat component base-popup

---
+++ resources/js/Components/UI/BasePopup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="mergedClass">
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+import { computed, defineProps } from 'vue'
+const props = defineProps({
+    class: {
+        type: String,
+        default: '',
+    }
+})
+const mergedClass = computed(() => {
+    const customClass = props.class || 'bg-white divide-y divide-gray-100 shadow-sm focus:!outline-none border border-text rounded-md'
+
+    return `absolute right-0 w-56 mt-1 origin-top-right ${customClass}`
+})
+</script>
