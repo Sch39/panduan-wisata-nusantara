b1c2beffa8b7322e21c82ac3d714dab50659d9cb
Sch39 <sukronraezpactoer@gmail.com>
2024-07-09 13:42:40 +0700

    jadikan jumbotron sebagai component di /UI

---
+++ resources/js/Components/UI/BaseJumbotron.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="relative bg-cover bg-center h-screen"
+        style="background-image: url('/assets/images/volcano-with-mist-sunset.jpg');">
+        <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+        <div class="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
+            <h1 v-if="!$slots.title && props.title" class="text-5xl font-bold mb-4">{{ props.title }}</h1>
+            <slot v-else name="title"></slot>
+            <p v-if="!$slots.description && props.description" class="text-lg mb-8 max-w-xl mx-auto">{{
+                props.description }}</p>
+            <slot v-else name="description"></slot>
+            <Link v-if="!$slots.link && props.link" :href="props.link"
+                class="px-6 py-3 bg-accent text-black text-lg rounded-full hover:opacity-80 transition duration-300 font-semibold">
+            {{ props.linkText }}</Link>
+            <slot v-else name="link"></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { defineProps } from "vue";
+const props = defineProps({
+    title: { type: String, required: false },
+    description: { type: String, required: false },
+    link: { type: String, required: false },
+    linkText: { type: String, required: false },
+})
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,21 +4,12 @@
         <title>{{ __('pages.home.title') }}</title>
         <meta head-key="description" name="description" :content="__('pages.home.meta_description')" />
     </Head>
+    <BaseJumbotron :title="__('pages.home.jumbotron_title')" :description="__('pages.home.jumbotron_description')"
+        :link="$useRoute('/dashboard')" :link-text="__('pages.home.jumbotron_button').toUpperCase()" />
 
-    <div class="relative bg-cover bg-center h-screen"
-        style="background-image: url('/assets/images/volcano-with-mist-sunset.jpg');">
-        <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
-        <div class="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
-            <h1 class="text-5xl font-bold mb-4">{{ __('pages.home.jumbotron_title') }}</h1>
-            <p class="text-lg mb-8 max-w-xl mx-auto">{{ __('pages.home.jumbotron_description') }}</p>
-            <Link :href="$useRoute('/dashboard')"
-                class="px-6 py-3 bg-accent text-black text-lg rounded-full hover:opacity-80 transition duration-300 font-semibold">
-            {{ __('pages.home.jumbotron_button') }}</Link>
-
-        </div>
-    </div>
 </template>
 
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import BaseJumbotron from './../Components/UI/BaseJumbotron.vue'
 </script>
