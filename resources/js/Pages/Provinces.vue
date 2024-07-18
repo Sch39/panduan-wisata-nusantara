439bf1dcac623301fd172a0c47c5f883d1e7d50d
Sch39 <sukronraezpactoer@gmail.com>
2024-07-18 16:29:22 +0700

    add - multi lang list daftar provinsi

--- resources/js/Pages/Provinces.vue
+++ resources/js/Pages/Provinces.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="p-4 mt-2">
+
+    <Head>
+        <title>{{ __('pages.provinces.title') }}</title>
+        <meta head-key="description" name="description" :content="__('pages.provinces.meta_description')" />
+    </Head>
+
+    <div class="p-4 mt-2 min-h-screen">
         <div class="flex items-center mb-5">
             <button class="focus:outline-none">
                 <i class='bx bx-arrow-back text-2xl'></i>
             </button>
-            <h2 class="text-2xl font-bold item ml-4">Daftar Provinsi di Indonesia</h2>
+            <h2 class="text-2xl font-bold item ml-4">{{ __('pages.provinces.content_title') }}</h2>
         </div>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
             <Link v-for="province in props.provinces" :key="province.code"
@@ -17,7 +23,7 @@
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import { Link, Head } from '@inertiajs/vue3'
 const props = defineProps({
     provinces: {
         type: Object,
